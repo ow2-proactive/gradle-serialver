c468c617f4badf57638dda0c541cc6be507e7c8a
Sergei Dolgov <Sergei.Dolgov@activeeon.com>
2014-03-07 11:12:48 +0100

    Gradle task: don't edit files in-place
     - spoon is not meant to edit files in place, sometimes the content can
       be lost
    
     - use tmp dir as output, then copy the files back
    
     - process all inputs

--- src/main/groovy/InsertSerialVersionUIDTask.groovy
+++ src/main/groovy/InsertSerialVersionUIDTask.groovy
@@ -1,17 +1,35 @@
 import org.gradle.api.logging.LogLevel
 import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.TaskAction
 
 class InsertSerialVersionUIDTask extends JavaExec {
     def input = project.sourceSets.main.java.srcDirs
 
     def InsertSerialVersionUIDTask() {
         setMain('spoon.Launcher')
-        setClasspath(project.rootProject.buildscript.configurations.classpath + project.sourceSets.main.runtimeClasspath)
-        setArgs(['--compliance', '6', '--fragments',
-                '-p', 'serialver.InsertSerialVersionUIDProcessor',
-                '-i', input.toArray()[0],
-                '--output', input.toArray()[0]])
+        setClasspath(project.rootProject.buildscript.configurations.classpath +
+                project.sourceSets.main.runtimeClasspath)
         logging.captureStandardOutput LogLevel.INFO
+    }
+
+    @TaskAction
+    public void exec() {
+        input.eachWithIndex { dir, i ->
+            setArgs(['--compliance', '6', '--fragments',
+                    '-p', 'serialver.InsertSerialVersionUIDProcessor',
+                    '-i', dir,
+                    '--output', temporaryDirForIndex(i)])
+            super.exec()
+        }
+        input.eachWithIndex { dir, i ->
+            ant.copy( todir: dir ) {
+                fileset( dir: temporaryDirForIndex(i) )
+            }
+        }
+    }
+
+    def temporaryDirForIndex(i) {
+        [temporaryDir, "serialver", i].join(File.separator)
     }
 
     def setSerialver(serialver) {
