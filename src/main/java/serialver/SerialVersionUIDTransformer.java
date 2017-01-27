cbf449d5e97e026baf6d8dc93638f9a1d6a17100
MaelAudren <mael.audren@activeeon.com>
2017-01-27 17:20:04 +0100

    apply coding rules

--- src/main/java/serialver/SerialVersionUIDTransformer.java
+++ src/main/java/serialver/SerialVersionUIDTransformer.java
@@ -1,47 +1,38 @@
 /*
- *  *
- * ProActive Parallel Suite(TM): The Java(TM) library for
- *    Parallel, Distributed, Multi-Core Computing for
- *    Enterprise Grids & Clouds
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
  *
- * Copyright (C) 1997-2015 INRIA/University of
- *                 Nice-Sophia Antipolis/ActiveEon
- * Contact: proactive@ow2.org or contact@activeeon.com
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
  *
- * This library is free software; you can redistribute it and/or
+ * This library is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation; version 3 of
+ * as published by the Free Software Foundation: version 3 of
  * the License.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * If needed, contact us to obtain a release under GPL Version 2 or 3
  * or a different license than the AGPL.
- *
- *  Initial developer(s):               The ProActive Team
- *                        http://proactive.inria.fr/team_members.htm
- *  Contributor(s):
- *
- *  * $$ACTIVEEON_INITIAL_DEV$$
  */
 package serialver;
 
-import com.darylteo.gradle.javassist.transformers.ClassTransformer;
+import java.io.Serializable;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtField;
 import javassist.NotFoundException;
 import javassist.build.JavassistBuildException;
 
-import java.io.Serializable;
+import com.darylteo.gradle.javassist.transformers.ClassTransformer;
 
 
 public class SerialVersionUIDTransformer extends ClassTransformer {
@@ -57,7 +48,6 @@ public class SerialVersionUIDTransformer extends ClassTransformer {
     public SerialVersionUIDTransformer(long serialVersionUIDValue) {
         this(serialVersionUIDValue, true, false);
     }
-
 
     public SerialVersionUIDTransformer(long serialVersionUIDValue, boolean overwrite, boolean forceUIDOnException) {
         this.serialVersionUIDValue = serialVersionUIDValue;
@@ -77,8 +67,7 @@ public class SerialVersionUIDTransformer extends ClassTransformer {
 
             if (!hasSerialVersionUIDField(clazz)) {
                 CtField field = new CtField(CtClass.longType, SERIALVERSIONUID_FIELD_NAME, clazz);
-                field.setModifiers(javassist.Modifier.STATIC | javassist.Modifier.PRIVATE |
-                        javassist.Modifier.FINAL);
+                field.setModifiers(javassist.Modifier.STATIC | javassist.Modifier.PRIVATE | javassist.Modifier.FINAL);
                 if (forceUIDOnException && isException(clazz)) {
                     clazz.addField(field, javassist.CtField.Initializer.constant(1L));
                 } else {
--- src/test/groovy/serialver/SerialVersionUIDTransformerTest.java
+++ src/test/groovy/serialver/SerialVersionUIDTransformerTest.java
@@ -1,17 +1,44 @@
+/*
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
+ *
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation: version 3 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ */
 package serialver;
+
+import static org.junit.Assert.*;
 
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.NotFoundException;
+
 import org.junit.Test;
+
 import serialver.tests.ExceptionSubClass;
 import serialver.tests.Interface;
 import serialver.tests.NotSerializableClass;
 import serialver.tests.SerialVersionUIDAlreadyDefined;
 import serialver.tests.SerializableClass;
 import serialver.tests.SerializableClassWithInheritance;
-
-import static org.junit.Assert.*;
 
 
 public class SerialVersionUIDTransformerTest {
@@ -20,17 +47,14 @@ public class SerialVersionUIDTransformerTest {
     public void shouldTransform_ClassIsSerializable() throws Exception {
         assertTrue(new SerialVersionUIDTransformer(42L).shouldTransform(getClazz(SerializableClass.class)));
 
-        assertTrue(new SerialVersionUIDTransformer(42L)
-                .shouldTransform(getClazz(SerializableClassWithInheritance.class)));
+        assertTrue(new SerialVersionUIDTransformer(42L).shouldTransform(getClazz(SerializableClassWithInheritance.class)));
 
-        assertTrue(new SerialVersionUIDTransformer(42L)
-          .shouldTransform(getClazz(ExceptionSubClass.class)));
+        assertTrue(new SerialVersionUIDTransformer(42L).shouldTransform(getClazz(ExceptionSubClass.class)));
     }
 
     @Test
     public void shouldTransform_SerialVersionUIDAlreadyDefined() throws Exception {
-        assertTrue(new SerialVersionUIDTransformer(42L).shouldTransform(
-          getClazz(SerialVersionUIDAlreadyDefined.class)));
+        assertTrue(new SerialVersionUIDTransformer(42L).shouldTransform(getClazz(SerialVersionUIDAlreadyDefined.class)));
     }
 
     @Test
